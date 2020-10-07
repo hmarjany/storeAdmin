2e76fb61ab5baa37ae074c1998fbbbefec84e9be
Hossein <h_marjany@yahoo.com>
2020-10-07 13:22:18 +0330

    fix bugs

--- src/app/server.ts
+++ src/app/server.ts
@@ -1,4 +1,4 @@
 export class server{
     // public static serverUrl = 'http://127.0.0.1:3100/';
-    public static serverUrl = 'http://api.hanamond.ir/';
+    public static serverUrl = 'https://api.hanamond.ir/';
 }
